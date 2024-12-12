b28a9b1de5874768af57f96fef9624dfe0aa201c
Humayra <95118185+Humayra7@users.noreply.github.com>
2024-12-12 19:31:29 +0600

    Add files via upload

---
+++ project_final/Backend/entities/message.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { User } from 'src/entities/user.entity';
+
+@Entity()
+export class messages {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => User, user => user.sentmessage)
+  senders: User;
+
+  @ManyToOne(() => User, user => user.receivedmessage)
+  receivers: User;
+
+ // @Column({
+  //  type: 'enum',
+  //  enum: ['pending', 'accepted', 'rejected'],
+   // default: 'pending',
+  //})
+  @Column({ type: 'varchar' })
+//status: string;
+  text: string;
+}
